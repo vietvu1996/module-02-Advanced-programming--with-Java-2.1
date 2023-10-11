e85521ef5da8ab5a8fc7b2432aa0fc5a5e60c363
unknown <shuhari0123@gmail.com>
2023-10-11 14:56:28 +0700

    thuchanhmang

---
+++ student-array/src/StudentArray.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class StudentArray {
+    public static void main(String[] args) {
+        String[] students = {"Christian", "Michael", "Camila", "Sienna", "Tanya", "Connor", "Zachariah", "Mallory", "Zoe", "Emily"};
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a name's student: ");
+        String input_name = scanner.nextLine();
+        boolean isExist = false;
+        for (int i = 0; i < students.length; i++) {
+            if(students[i].equals(input_name)){
+                isExist = true;
+                System.out.println("Position of the students in the list " + input_name + " is: " + (i + 1));
+                break;
+            }
+        }
+        if(!isExist){
+            System.out.println("Not found " + input_name + " in the list");
+        }
+    }
+}
