a17b938f5c1176245a6d5fd5f5e2d78a1108022c
unknown <shuhari0123@gmail.com>
2023-10-21 21:22:06 +0700

    thuchanhqueue

---
+++ lession-11-stack-and-queue/deploy-Queue-to-use-LinkedList/src/MyLinkedListQueue.java
@@ -0,0 +1,29 @@
+public class MyLinkedListQueue {
+    private Node head;
+    private Node tail;
+
+    public MyLinkedListQueue(){
+
+    }
+
+    public void enqueue(int key){
+        Node temp = new Node(key);
+        if (this.tail == null) {
+            this.tail = head;
+            return;
+        }
+        this.tail.next = temp;
+        this.tail = temp;
+    }
+
+    public Node dequeue(){
+        if(this.head == null){
+            return null;
+        }
+        Node temp = this.head;
+        this.head = this.head.next;
+        if(this.head == null)
+            this.tail = null;
+        return temp;
+    }
+}
